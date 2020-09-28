04cf15b748e3752e1351485f7cacf37e140d92b4
oliver.bannister <oliver.bannister@roxorgaming.co.uk>
2020-09-28 15:09:00 +0100

    changed innerhtml to value

--- src/main.js
+++ src/main.js
@@ -2,14 +2,13 @@ import { LexicalAnalyser } from './LexicalAnalyser';
 import { parse } from './parser';
 import { Evaluator } from './Evaluator';
 
-
 const lexicalAnalyser = new LexicalAnalyser();
 const evaluator = new Evaluator();
 window.onload = (() => {
     const button = document.getElementById('parse');
     button.onclick = () => {
         const textBox = document.getElementById('textBox');
-        const tokens = lexicalAnalyser.getTokens(textBox.innerHTML);
+        const tokens = lexicalAnalyser.getTokens(textBox.value);
         const abstractSyntaxTree = parse(tokens);
         const output = evaluator.evaluateExpression(abstractSyntaxTree);
         const heading = document.getElementById('outputNode');
